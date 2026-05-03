0db9c3664b42c0ff8c5cf2e4afc2c5e2c663bd98
DaddyDuong <vxduong1504@gmail.com>
2026-05-03 22:45:36 +0700

    feat: add additional image fields to products table

---
+++ backend/prisma/migrations/20260503120000_add_product_image_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "products" ADD COLUMN "image_url_2" TEXT;
+ALTER TABLE "products" ADD COLUMN "image_url_3" TEXT;
+ALTER TABLE "products" ADD COLUMN "image_url_4" TEXT;
+ALTER TABLE "products" ADD COLUMN "image_url_5" TEXT;
